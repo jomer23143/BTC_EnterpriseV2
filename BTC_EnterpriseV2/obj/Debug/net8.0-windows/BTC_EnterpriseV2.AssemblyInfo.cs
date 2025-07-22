167c1b6945a49a4587e7e66ae8c65f267952bcb2
Jomer Alo <jalo@btcpower.com>
2025-07-22 16:31:51 +0800

    new update 1

--- BTC_EnterpriseV2/obj/Debug/net8.0-windows/BTC_EnterpriseV2.AssemblyInfo.cs
+++ BTC_EnterpriseV2/obj/Debug/net8.0-windows/BTC_EnterpriseV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTC_EnterpriseV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8155aa73da1dba8aeab5b0f4172c3303171df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6e69098adfe200869418dfe251e273fe7b81f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTC_EnterpriseV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTC_EnterpriseV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
